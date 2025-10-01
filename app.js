6b5736afdb0422fdb214a511147a3a81653b4a36
dartboy <nmdart@gmail.com>
2025-10-01 16:22:19 -0400

    update

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector('.vimeoContainer');
-    const iframe = container.querySelector('.vimeoVideo');
-    const bgClass = document.querySelector('.bgPoster');
-    bgClass.classList.add('is-ready');
-    iframe.addEventListener('load', () => {
-        container.classList.add('is-ready');
-        
+    const videos = document.querySelectorAll(".bgVid");
+    console.log(videos);
+    let current = 0;
+    
+    videos[current].play();
+
+    videos.forEach(video => {
+        video.pause();
+        video.currentTime = 0;
+        video.classList.remove("active");
     });
+    
+    videos[current].classList.add("active");   
+    videos[current].play();
+
+    videos.forEach((video, i) => {
+       
+        video.addEventListener("ended", () => {
+            console.log("HELLO");
+
+            videos[i].pause();
+            videos[i].currentTime = 0;
+            videos[i].classList.remove("active");
+            
+       
+            current = (i + 1) % videos.length;
+
+            videos[current].classList.add("active");
+            videos[current].play();
+    });
+   });
+
 });
 
